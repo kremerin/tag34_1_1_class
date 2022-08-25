b0d66c4d43dfec188601fb834249188d8a6678f5
Kremerin <110621622+kremerin@users.noreply.github.com>
2022-08-25 18:13:15 +0200

    Vers1

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let bigHeadline = document.getElementById("headline");
+let listElement1 = document.getElementById("list1");
+let listElement2 = document.getElementById("list2");
+let listElement3 = document.getElementById("list3");
+let listElement4 = document.getElementById("list4");
+
+function change() {
+    bigHeadline.classList.add("bigHeadline");
+    listElement1.classList.add("listElement1")
+    listElement2.classList.add("listElement2")
+    listElement3.classList.add("listElement3")
+    listElement4.classList.add("listElement4")
+}
